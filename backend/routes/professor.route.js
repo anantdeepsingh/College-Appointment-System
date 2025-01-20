45d228164f35a483a999331a84c4f12cb04cf748
anantdeepsingh <anantdeep2004@gmail.com>
2025-01-20 17:34:48 +0530

    professor route updated

--- backend/routes/professor.route.js
+++ backend/routes/professor.route.js
@@ -4,5 +4,8 @@ import { addAvailability, getAvailability,viewBookings,cancelAppointment, delete
 const router=express.Router();
 
 router.route("/availability").post(isAuthenticated,addAvailability);
-
+router.route("/scheduleavailable").get(isAuthenticated,getAvailability);
+router.route("/bookings").get(isAuthenticated,viewBookings);
+router.route("/bookings/:appointmentId/cancel").put(isAuthenticated,cancelAppointment);
+router.route("/availability/:slotId").delete(isAuthenticated,deleteAppointment);
 export default router;
